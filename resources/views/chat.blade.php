5d8f5bcd3c67e68521c579329794ae64dd33e9f7
Muhamad Ilham Arrouf <ilham.arrouf@gmail.com>
2017-09-25 23:08:35 +0700

    setup project

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div class="row" id="app">
+            <ul class="list-group offset-4 col-4">
+                <li class="list-group-item active">Chat room</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+                <input class="form-control" type="text" placeholder="Type your message here...">
+            </ul>
+        </div>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
