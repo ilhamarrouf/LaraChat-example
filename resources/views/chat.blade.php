10a75df2ffd251b2083657764750ef8ee9b40d8f
Muhamad Ilham Arrouf <ilham.arrouf@gmail.com>
2017-09-26 18:14:56 +0700

    VueJS chat scroll

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -18,9 +18,11 @@
             <div class="offset-4 col-4">
                 <li class="list-group-item active">Chat room</li>
 
-                <ul class="list-group">
+                <ul class="list-group" v-chat-scroll>
                     <message
                         v-for="value in chat.message"
+                        :key=value.index
+                        color=success
                     >
                         @{{ value }}
                     </message>
